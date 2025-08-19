f464693675ec66d09e0092652dcd860c8eb8cd9a
Sreekanth S <sreekanth.sh@ewallsolutions.com>
2025-08-19 14:14:53 +0530

    a

--- assets/notify-overlay.js
+++ assets/notify-overlay.js
@@ -1,9 +1,9 @@
 jQuery(function($) {
 
-
     // Close popup
     $(".notify-button-product-card").click(function(){
-        $('.notify-form').css('display', 'block'); // Hide
+        $('.notify-form').css('display', 'flex');
+        $('.notify-form').css('height', '100%'); // Hide
     });
     $(".modal__close-button").click(function(){
         $('.notify-form').css('display', 'none'); // Hide
